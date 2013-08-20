0c353201f96fadef8da9be3d20853821de1150a3
Fan Dong <fdong@research.baycrest.org>
2013-08-20 10:13:46 -0400

    adding templates

---
+++ app/models/Data.java
@@ -0,0 +1,13 @@
+package models;
+
+import java.util.*;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: fdong
+ * Date: 8/20/13
+ * Time: 9:53 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Data {
+}
